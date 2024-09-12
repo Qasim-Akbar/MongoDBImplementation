3d02dedcdc6c79d8b5752dea62453f07bc05968d
Qasim-Akbar <qasim.akbar343@gmail.com>
2024-09-12 12:09:03 +0500

    Add project files.

---
+++ MongoDBImplementation/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+//Define the mongoDb connection string and database name
+string connectionString = "mongodb://localhost:27017/";
+string databaseName = "EDUDB";
+
+//Create a MongoClient to connect to the mongo server
+MongoClient client = new MongoClient(connectionString);
+
+//Access the database
+IMongoDatabase database =  client.GetDatabase(databaseName);
+
+Console.WriteLine("Connected to Database");
+
+var collection = database.GetCollection<BsonDocument>("EDUDBCollection");
+var document = new BsonDocument
+{
+    {"name", "Peter" },
+    {"age", 32 },
+    {"salary", 25000 }
+};
+
+collection.InsertOne(document);
+Console.WriteLine("Record Added to Database");
+
+//var filter = Builders<BsonDocument>.Filter
